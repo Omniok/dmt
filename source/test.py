c69f01de732ed621bbd786e50a404678c1031708
Omniok <joseph.haney.bus@gmail.com>
2021-01-03 16:12:08 -0600

    Initial commit for flask rest api

---
+++ source/test.py
@@ -0,0 +1,10 @@
+import requests
+
+BASE = "http://127.0.0.1:80/"
+
+response = requests.get(BASE + "enemy/1", {})
+input()
+response = requests.delete(BASE + "enemy/4", {})
+input()
+
+print(response.json())
